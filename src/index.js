6b0ffdb903d5ad1b0b9726e85e55c34252f42aa5
Stephane Mansour <stephane.mansour@gmail.com>
2017-07-14 11:51:24 +0100

    adding React components

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './components/Header'
+
+const App = () => {
+  return(
+   <div className = "App">
+     <Header message="Naming Contests"/>
+      <div>
+        ...
+      </div>
+   </div>
+  );
+};
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
