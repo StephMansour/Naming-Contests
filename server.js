37f46c93f0d8aeea26e1b6099f3cb95d1bcd8c2d
Stephane Mansour <stephane.mansour@gmail.com>
2017-07-14 19:10:46 +0100

    rendering on the server and fixing checksum problem

--- server.js
+++ server.js
@@ -13,12 +13,15 @@ server.use(sassMiddleware({
 
 server.set('view engine', 'ejs');
 
-import './serverRender'
+import serverRender from './serverRender'
 
 server.get('/', (req, res) => {
-  res.render('index', {
-    content: 'Hello Express and EJS!'
-  });
+  serverRender()
+    .then(content => {
+      res.render('index', {
+        content
+      });
+    });
 });
 
 server.use('/api', apiRouter);
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import axios from 'axios';
 import Header from './Header'
 import ContestPreview from './ContestPreview'
 
@@ -7,16 +6,10 @@ import ContestPreview from './ContestPreview'
 class App extends React.Component {
   state = {
     pageHeader: 'Naming Contests',
-    contests: []
+    contests: this.props.initialContests
   };
   componentDidMount() {
-    axios.get('/api/contests')
-      .then(resp => {
-        this.setState({
-          contests: resp.data.contests
-        });
-      })
-      .catch(console.error)
+
   }
   componentWillUnmount() {
     // clean timers, listeners
